f6d234d5c8da3ddb2fec0a24da2bb495ad536d1c
leonel ka <waboleonel@gmail.com>
2024-04-20 21:03:37 +0100

    Error when using beans configuration

--- store-microservice/src/main/java/com/wbt/storemicroservice/config/KafkaConsumerConfiguration.java
+++ store-microservice/src/main/java/com/wbt/storemicroservice/config/KafkaConsumerConfiguration.java
@@ -2,48 +2,47 @@ package com.wbt.storemicroservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfiguration {
 
-    @Autowired
-    Environment env;
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServers;
 
-    @Bean
-    public Map<String, Object> consumerConfig() {
-        final var config = new HashMap<String, Object>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.consumer.bootstrap-servers"));
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, env.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, env.getProperty("spring.kafka.consumer.group-id"));
+    @Value(value = "${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private String trustedPackages;
 
-        return config;
-    }
+    @Value(value = "${spring.kafka.consumer.group-id}")
+    private String groupId;
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+        final Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackages);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(final ConsumerFactory<String, Object> consumerFactory) {
+    public ConcurrentKafkaListenerContainerFactory<String, Object> containerFactory(ConsumerFactory<String, Object> consumerFactory) {
         final var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
         factory.setConsumerFactory(consumerFactory);
-
         return factory;
     }
 }
--- store-microservice/src/main/java/com/wbt/storemicroservice/handler/ProductCreatedEventHandler.java
+++ store-microservice/src/main/java/com/wbt/storemicroservice/handler/ProductCreatedEventHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +14,7 @@ public class ProductCreatedEventHandler {
     private final static Logger LOGGER = LoggerFactory.getLogger(ProductCreatedEventHandler.class);
 
     @KafkaHandler
-    public void handler(final @Payload ProductCreatedEvent productCreatedEvent) {
+    public void handler(final ProductCreatedEvent productCreatedEvent) {
         LOGGER.info(">>>> New product created " + productCreatedEvent);
     }
 
