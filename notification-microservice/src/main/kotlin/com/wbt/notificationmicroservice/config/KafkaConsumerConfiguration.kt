5b812563a44b9e31b94c490b2faa16c257d73438
leonel ka <waboleonel@gmail.com>
2024-04-20 21:36:34 +0100

    Error persists

--- notification-microservice/src/main/kotlin/com/wbt/notificationmicroservice/config/KafkaConsumerConfiguration.kt
+++ notification-microservice/src/main/kotlin/com/wbt/notificationmicroservice/config/KafkaConsumerConfiguration.kt
@@ -2,37 +2,41 @@ package com.wbt.notificationmicroservice.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.support.serializer.JsonDeserializer
 
-//@Configuration
+@EnableKafka
+@Configuration
 class KafkaConsumerConfiguration {
 
-//    @Autowired
-    private lateinit var environment: Environment
+    @Value(value = "\${spring.kafka.consumer.bootstrap-servers}")
+    private lateinit var brokers: MutableList<String>
 
-//    @Bean
+    @Value(value = "\${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private lateinit var trustedPackages: String
+
+    @Value(value = "\${spring.kafka.consumer.group-id}")
+    private lateinit var groupId: String
+
+    @Bean
     fun consumerFactory(): ConsumerFactory<String, Any> {
         val config: MutableMap<String, Any> = HashMap()
-        environment.getProperty("spring.kafka.consumer.bootstrap-servers")
-            ?.let { servers -> config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers) }
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
         config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
-        environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages")
-            ?.let { pkg -> config.put(JsonDeserializer.TRUSTED_PACKAGES, pkg) }
-        environment.getProperty("spring.kafka.consumer.group-id")
-            ?.let { config.put(ConsumerConfig.GROUP_ID_CONFIG, it) }
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackages)
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
 
         return DefaultKafkaConsumerFactory(config)
     }
 
-//    @Bean
+    @Bean
     fun kafkaListenercontainerFactory(consumerFactory: ConsumerFactory<String, Any>): ConcurrentKafkaListenerContainerFactory<String, Any> {
         return ConcurrentKafkaListenerContainerFactory<String, Any>().apply {
             setConsumerFactory(consumerFactory)
